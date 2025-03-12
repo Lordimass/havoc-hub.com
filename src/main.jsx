82dea73cc915e89ebba8d9a8769cb6886b639d0b
Lordimass <sam@tchrin.net>
2025-03-12 12:57:08 +0000

    Note to future self
    Added a note to myself on how to deploy new versions of the site should I forget how to do that at some point in future.

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+/**
+ * Hello future me!
+ * I'm writing this in the eventuality that I forget how to deploy new versions
+ * of the site in the future, which is very likely, so I'm leaving this note in
+ * the most obvious place I can think of :)
+ * 
+ * To deploy, simply run `npm run deploy` in the root directory of the repo,
+ * everything else is automatic and Github should publish the site within ~1min 
+ */
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4"
