b418a8882ac6c00c7e0f70b6cb054e1c02c02d29
Lordimass <lordimass@lordimass.net>
2025-10-28 14:00:59 +0000

    Logo Tweaks

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,8 +6,16 @@ export default function Home() {
     return (
         <div className="content home-content">
             <div className="content-box">
-                <div id="home-page-logo"><img src={TRANSPARENT_PRIMARY_LOGO}/></div>
-                <h1>Under Construction!</h1>
+                <div id="home-page-logo">
+                    <img
+                        src={TRANSPARENT_PRIMARY_LOGO}
+                        alt={`
+                        A logo in a bubbly font which reads 'Havoc'. There are horns on the H, and the c flows into
+                        an imp tail which curls over the top of the logo. The whole word has a cartoony fiery red
+                        background
+                        `}
+                    />
+                </div>
             </div>
         </div>
     );
